227d796511438c6dd2854960a803532e525e9ebc
David Parry <david.parry@suranyami.com>
2017-10-24 09:21:16 +1100

    Add skeleton for cell, grid

---
+++ src/components/Cell.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+
+</template>
+
+<script lang="coffee">
+export default ->
+  data: ->
+    []
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .grid
+    li(v-for='name in fruit') {{name}}
+    #drawing
+</template>
+
+<script lang="coffee">
+
+</script>
+
+<style lang="scss">
+
+</style>
