df9f81215d7450c971ab0d470aa6b13147f989a3
David Parry <david.parry@suranyami.com>
2017-10-24 09:20:53 +1100

    Add simple ugly about box

---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  h1 Mazes implemented in Vue.js using CoffeeScript, Pug, SASS and SVG.
+</template>
+
+<script lang="coffee">
+
+</script>
+
+<style lang="scss">
+
+</style>
