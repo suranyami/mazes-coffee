85216499293f73275d42a3dcd86c103a2297cccc
David Parry <david.parry@suranyami.com>
2017-10-24 09:22:02 +1100

    Add about, maze routes

---
+++ src/router/NavBar.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+ul
+  li
+    router-link(to="/about") About
+  li
+    router-link(to="/maze") Maze
+</template>
+
+<script lang="coffee">
+export default
+  name: "nav-bar"
+</script>
+
+<style lang="scss">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Maze from '@/components/Maze'
+import About from '@/components/About'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Maze',
-      component: Maze
-    }
+    {path: '/', alias: '/maze', component: Maze},
+    {path: '/about', name: 'About', component: About},
   ]
 })
